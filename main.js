b61f8551eb3750db6091094c819b2498d44f2dde
iuri-medina <iurimedina@gmail.com>
2022-08-17 23:34:41 -0300

    Mudanças de estilo e adição de data

--- main.js
+++ main.js
@@ -1,7 +1,48 @@
+const date = new Date;
+const today = date.toDateString();
+splitedToday = today.split(" ");
+let dayString = splitedToday[0];
+const dayNumber = splitedToday[2];
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+if(dayString === 'Sun') {
+    dayString = days[0];
+}
+else if(dayString === 'Mon') {
+    dayString = days[1];
+}
+else if(dayString === 'Tue') {
+    dayString = days[2];
+}
+else if(dayString === 'Wed') {
+    dayString = days[3];
+}
+else if(dayString === 'Thu') {
+    dayString = days[4];
+}
+else if(dayString === 'Fri') {
+    dayString = days[5];
+}
+else {
+    dayString = days[6];
+}
+
+const dateTitle = document.querySelector('.date');
+if (dayNumber === '1') {
+    dateTitle.innerHTML = `${dayString}, ${dayNumber}st`;
+}
+else if(dayNumber === '2' || dayNumber === '3') {
+    dateTitle.innerHTML = `${dayString}, ${dayNumber}nd`;
+} else {
+    dateTitle.innerHTML = `${dayString}, ${dayNumber}th`;
+}
+
+
+
 const input = document.querySelector('#toDoInput');
 const addButton = document.querySelector('.add-task');
 const list = document.querySelector('#task-list');
-
 
 addButton.addEventListener("click", () => {
     if(input.value === "" || input.value === " ") {
@@ -11,26 +52,13 @@ addButton.addEventListener("click", () => {
         let task = document.createElement('li');
         task.innerHTML = input.value;
         list.appendChild(task);
+        input.value = "";
 
         let remove = document.createElement('button');
         remove.classList.add('del-btn')
         remove.onclick = () => {
-            list.removeChild(task);
-            list.removeChild(remove);
+            setTimeout(() => {list.removeChild(task)}, 150);
         };
         task.appendChild(remove);
     }
 });
-
-
-const date = new Date;
-
-const today = date.toDateString();
-splitedToday = today.split(" ");
-const dayString = splitedToday[0];
-const month = splitedToday[1];
-const dayNumber = splitedToday[2];
-const year = splitedToday[3];
-
-console.log(dayString, dayNumber, month, year);
-
